cc64f0ca9d1b9af79094979157738523fc79ff63
igarashi20176 <106209681+igarashi20176@users.noreply.github.com>
2023-01-07 20:14:37 +0900

    Update mudp_recv_latest.c

--- mudp_recv_latest.c
+++ mudp_recv_latest.c
@@ -11,8 +11,9 @@ const char * ssid = "elecom-68d043";
 const char * password = "33dcu4jvm9d9";
 // const char * password = "11n-ky56$HDxgp";
 const int port = 1234;
+
+IPAddress src_ip;
 time_t st = 0;
-time_t et = 0;
 
 AsyncUDP udp;
 
@@ -20,11 +21,10 @@ WiFiServer server(port);
 
 
 // sequence array & push method
-int seq_ary[120];
+int seq_ary[150] = {0};
 int max_recv_size = 0;
 int tmp = 0;
 int ary_size = 0;
-int count = 0;
 
 int compareInt(const void* a, const void* b) {
     int aNum = *(int*)a;
@@ -39,7 +39,6 @@ void printArray(const int* array, size_t size) {
         printf("%d ", array[i]);
     }
 }
-
 
 void setup()
 {
@@ -62,18 +61,18 @@ void setup()
     if (udp.listenMulticast(IPAddress(239,1,2,3), port)) {
         Serial.print("UDP Listening on IP: ");
         Serial.println(WiFi.localIP());
-        udp.onPacket([](AsyncUDPPacket packet) {            
+        udp.onPacket([](AsyncUDPPacket packet) {   
+            
+            src_ip = packet.remoteIP();
+
             char recvd[packet.length()];
             memcpy(recvd, packet.data(), packet.length());
-
-            printf("recv: %s", recvd);
-            printf("\n");
 
             char *token;
             token = strtok(recvd, ":");
             
             max_recv_size = atoi(token);
-                  
+
             token = strtok(NULL, ":");
             
             int add_item = atoi(token);
@@ -81,65 +80,7 @@ void setup()
             seq_ary[ary_size] = add_item;
             ary_size++;
 
-            count++;
-
-            // reply to the client
-            if (count == max_recv_size) {
-              printf("\n");
-
-              qsort(seq_ary, ary_size, sizeof(int), compareInt);
-
-              printArray(seq_ary, ary_size);
-
-              printf("\n");
-
-              // search loss packets
-              char loss[120] = "";
-              char buf[4];
-
-              // recv packet 0 case is Abbreviated...
-
-              // count...seq counter
-              // ex) seq_ary[3,4,5,6,7] / [1,2] loss search
-              if ( seq_ary[0] != 1 ) {
-                for ( count=1; count < seq_ary[0]; count++ ) {
-                  snprintf(buf, sizeof(buf), "%d", count);
-                  strcat(loss, buf);
-                  strcat(loss, ",");
-                }
-              }
-
-              count = seq_ary[0] + 1;
-              for ( tmp=1; tmp < ary_size; tmp++ ) {
-                while ( count < seq_ary[ary_size-1] ) {
-                  if ( seq_ary[tmp] == count ) {
-                      count++;
-                      break;
-                  } else {
-                      snprintf(buf, sizeof(buf), "%d", count);
-                      strcat(loss, buf);
-                      strcat(loss, ",");
-                      count++;
-                  }
-                }
-              }
-              
-              // ex) seq_ary[3,4,5,6,7] / [8,9,10] loss search
-              // 10 = 最大シーケンス番号
-              if ( seq_ary[ary_size-1] != max_recv_size ) {
-                for ( count=seq_ary[ary_size-1]+1; count <= 10; count++ ) {
-                    snprintf(buf, sizeof(buf), "%d", count);
-                    strcat(loss, buf);
-                    strcat(loss, ",");
-                }
-              }
-              
-              packet.print(loss);
-              Serial.printf("%s sended!\n", loss);
-              
-              count = 0;
-              ary_size = 0;
-            }
+            st = time(NULL);
         });
     }
 }
@@ -148,38 +89,92 @@ void setup()
 // TCP送受信部分
 void loop()
 {
-    if ( st != 0 && et - et > 2 ) {
+    if ( ary_size != max_recv_size && st && (time(NULL) - st) > 1 ) {
+
+      qsort(seq_ary, ary_size, sizeof(int), compareInt);
+      
+      // search loss packets
+      char loss[200] = "";
+      char buf[6];
+      int counter = 0;
+
+      // recv packet 0 case is Abbreviated...
+
+      // count...seq counter
+      // ex) seq_ary[3,4,5,6,7] / [1,2] loss search
+
+      if ( seq_ary[0] != 1 ) {
+        for ( counter=1; counter < seq_ary[0]; counter++ ) {
+          snprintf(buf, sizeof(buf), "%d", counter);
+          strcat(loss, buf);
+          strcat(loss, ",");
+        }
+      }
+
+      counter = seq_ary[0] + 1;
+      for ( tmp=1; tmp < ary_size; tmp++ ) {
+        while ( counter < seq_ary[ary_size-1] ) {
+          if ( seq_ary[tmp] == counter ) {
+              counter++;
+              break;
+          } else {
+              snprintf(buf, sizeof(buf), "%d", counter);
+              strcat(loss, buf);
+              strcat(loss, ",");
+              counter++;
+          }
+        }
+      }
+      
+      // ex) seq_ary[3,4,5,6,7] / [8,9,10] loss search
+      // 10 = 最大シーケンス番号
+      if ( seq_ary[ary_size-1] != max_recv_size ) {
+        for ( counter=seq_ary[ary_size-1]+1; counter <= max_recv_size; counter++ ) {
+            snprintf(buf, sizeof(buf), "%d", counter);
+            strcat(loss, buf);
+            strcat(loss, ",");
+        }
+      }
+      
 
       WiFiClient client = server.available();
+      client.connect(src_ip, port);
+      
+      client.println(loss);
+      Serial.printf("%s sended!\n", loss);
 
       if ( client ) {
         Serial.println("New Client Created");
+
         while ( client.connected() ) {
           // recv packet loss data from server
           int size = client.available();
-          // if( size ) {
-          //   Serial.println();
-          //   Serial.println("size:");
-          //   Serial.println(size);
+          if( size ) {
+            // Serial.printf("size: %d\n", size);
 
-          //   for (int i = 0; i < size; i++) {
-          //     // uint8_t...8ビット整数型
-          //     // c 10進数. ex) M = 77 = 0x4c  
-          //     uint8_t c = client.read();
-          //     // 1byteを1文字に変換
-          //     Serial.printf("get: %c\n", c);
-          //   }
+            // for (int i = 0; i < size; i++) {
+              // uint8_t...8ビット整数型
+              // c 10進数. ex) M = 77 = 0x4c  
+              // uint8_t c = client.read();
+              // 1byteを1文字に変換
+              // Serial.printf("get: %c\n", c);
 
-            // char input[256];
-            // memset(input, 0, sizeof(input));
-            // client.read((uint8_t*)input, sizeof(input) - 1);
-            // Serial.printf("get: [%s]\n", input);
-          // }
-          delay(1);
+              char input[256];
+              memset(input, 0, sizeof(input));
+              client.read((uint8_t*)input, sizeof(input) - 1);
+            }
+            delay(1);
+          }
+          client.stop();
+          Serial.println("client.stop");  
+        }        
+        for (int i = 0; i < ary_size; ++i) {
+          seq_ary[i] = 0;
         }
-        // server.write();
-        client.stop();
-        Serial.println("client.stop");
-      }
-   }
+
+        st = 0;
+        max_recv_size = 0;
+        ary_size = 0;
+    }
 }
+
